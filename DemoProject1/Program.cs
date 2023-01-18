059b3d22f90ff4757f4068e2bc958bc7976c4daa
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-18 15:06:58 +0530

    [Sachin] Adding DemoProject1

---
+++ DemoProject1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoProject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to new Project C sharp basic programs");
+            Console.ReadLine();
+        }
+    }
+}
