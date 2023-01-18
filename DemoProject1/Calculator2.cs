9fe25c9bdef7b86c6c0ec8023a5ad66b80c2f1c9
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-18 18:01:30 +0530

    [Sachin] Adding Addition,Subtraction,Multiplictation,Dividing,Functionality

---
+++ DemoProject1/Calculator2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject1
+{
+    public class Calculator2
+    {
+        // Create the instance variables inside the class
+        public int first, second, sum;
+
+        /// <summary>
+        /// initializes a new instance of the <see cref="Calculator2"/> class.
+        /// </summary>
+        /// <param name="firstNumb"></param>
+        /// <param name="secondNumb"></param>
+        public Calculator2(int firstNumb, int secondNumb)
+        {
+            first = firstNumb;
+            second = secondNumb;
+        }     
+
+        /// <summary>
+        /// Addition of 2 number to get sum
+        /// </summary>
+        public void Addition()
+        {
+            sum = first + second;
+            Console.WriteLine("Sum:" + sum);                            
+        }
+        public void Subtraction()
+        {
+            sum = Math.Abs (first - second);
+            Console.WriteLine("Subtraction:" + sum);
+        }
+        public void Multiplication()
+        {
+            sum = first * second;
+            Console.WriteLine("Multiplication:" + sum);
+        }
+        public void Dividing()
+        {
+            sum = first / second;
+            Console.WriteLine("Dividing:" + sum);
+        }
+        
+            
+    }
+}
--- DemoProject1/Program.cs
+++ DemoProject1/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace DemoProject1
+﻿
+
+namespace DemoProject1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to new Project C sharp basic programs");
+            Console.WriteLine("Please eter the first number");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            Calculator2 myObj = new Calculator2(first,second);
+            myObj.Addition();
+
+            Console.WriteLine("Please enter the first number");
+            int first1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            int second1 = Convert.ToInt32(Console.ReadLine());
+
+            Calculator2 myObj1 = new Calculator2(first1, second1);
+            myObj1.Subtraction();
+
+            Console.WriteLine("Please enter the first number");
+            int first2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number");
+            int second2 = Convert.ToInt32(Console.ReadLine());
+
+            Calculator2 myObj2 = new Calculator2(first2, second2);
+            myObj2.Multiplication();
+
+            Console.WriteLine("Please enter the first numbert");
+            int first3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Plese enter the second number");
+            int first4 = Convert.ToInt32(Console.ReadLine());
+
+            Calculator2 myObj3 = new Calculator2(first3, first4);
+            myObj3.Dividing();
             Console.ReadLine();
         }
     }
