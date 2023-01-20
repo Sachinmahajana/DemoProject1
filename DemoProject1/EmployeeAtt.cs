c7f2163d854e5fe54204dc8176936a328f463171
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-20 16:03:30 +0530

    [Sachin] Adding Usecase problem

---
+++ DemoProject1/EmployeeAtt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject1
+{
+    internal class EmployeeAtt
+    {
+        int isPresent = 1;
+        
+        public void EmployeeAttendence()
+        {
+            Random myObj4 = new Random();
+            int empAttendence = myObj4.Next(0, 2);
+
+            if (empAttendence == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
